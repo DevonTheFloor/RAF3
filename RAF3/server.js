471f0a71b4df748c347a016e6cad4177a5c3951e
DevonTheFloor <devonthefloor@free.fr>
2020-09-04 22:58:02 +0200

    set port

--- RAF3/server.js
+++ RAF3/server.js
@@ -1,6 +1,6 @@
 const http = require('http');
 const app = require('./app');
-app.set('port',process.env.PORT ||4444);
+app.set('port',process.env.PORT ||80);
 const server = http.createServer(app);
 console.log('Connexion: ok, ecoute port 80 (j\'espere)');
-server.listen(process.env.PORT || 4444);
+server.listen(process.env.PORT || 80);
